8f92f8418128625c1a1b4c1b702f921046e63062
AndresCabralTB <andres.cabralrs@udlap.mx>
2024-04-23 18:07:32 -0600

    Initial Commit

---
+++ Coppel_Project/Coppel_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coppel_ProjectApp.swift
+//  Coppel_Project
+//
+//  Created by Andres Cabral on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Coppel_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
